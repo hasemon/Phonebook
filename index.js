2b24bc5d02ab1385532ecde70d6e04d005b19d53
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-08-02 10:36:09 +0600

    uibuild

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 
+
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 morgan.token('body', (req, res) =>
